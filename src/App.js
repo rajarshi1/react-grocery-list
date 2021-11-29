65316b1389f0f897b82f973209d682d3129e8d8a
rajarshi1 <ghoshal.rajarshi@gmail.com>
2021-11-29 12:17:50 +0530

    works but has some bugs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,84 @@
-import React, { useState, useEffect, createContext} from 'react';
+import React, { useState, useEffect, createContext,useContext} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import './index.css';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ItemList from "./components/ItemList";
-import Form from "./components/Form";
+// import Form from "./components/Form";
 
 
 export const GroceryContext = createContext();
 console.log(GroceryContext);
+
+const Form = () => {
+
+		var {items,inputValue,setInputValue,setItems,setTotalItemCount} = useContext(GroceryContext)
+
+	// useEffect(() => {
+	//     window.addEventListener('keydown', (event) => {
+	//       if(event.key==='Enter'){
+	//           handleAddButtonClick()
+	//       }
+	//     });
+	// }, [inputValue]);
+
+
+	const handleAddButtonClick = () => {
+
+		 inputValue = inputValue.trim();
+
+		if (inputValue===""){
+			alert('Please enter a valid input');
+			return;
+		}
+		
+		const newItem = {
+			itemName: inputValue,
+			quantity: 1,
+			isSelected: false,
+		};
+
+		const newItems = [...items, newItem];
+		let itemArray = items.map(({ itemName }) => itemName)
+
+		if(itemArray.includes(inputValue)){
+			handleQuantityIncrease(itemArray.indexOf(inputValue))
+			setInputValue('');
+			return;
+		}
+		else{
+			setItems(newItems);
+			setInputValue('');
+			calculateTotal();
+		}	
+	};
+
+	const handleQuantityIncrease = (index) => {
+		const newItems = [...items];
+		newItems[index].quantity++;
+		setItems(newItems);
+		calculateTotal();
+	};
+
+
+	const calculateTotal = () => {
+		const totalItemCount = items.reduce((total, items) => {
+			return total + items.quantity;
+		}, 0);
+		setTotalItemCount(totalItemCount);
+	};
+
+	return (
+		<>
+		<div className='add-item-box'>
+			<input required="required" value={inputValue} onKeyPress={(event)=>{if(event.key==="Enter")handleAddButtonClick()}} onChange={(event) => setInputValue(event.target.value)} required="required" className='add-item-input' placeholder='Add an item...' />
+			<FontAwesomeIcon icon={faPlus} onClick={() => handleAddButtonClick()} />
+			{/* <button className="button-add" onClick={()=>handleAddButtonClick()}>Add</button> */}
+		</div>
+		</>
+	);
+};
 
 export const App = () => {
 
